c1f06c776fece38e4a686520d7a0a23772ac0e84
Tiago Freitas <tiago10cf@hotmail.com>
2021-03-28 02:19:38 +0100

    mais ou menos

--- b.py
+++ b.py
@@ -1,8 +1,46 @@
 import sys
 import re
 
+def pertence(elem,lista):
+    for x in lista:
+        if x == elem:
+            return True
+    return False
+
+def delete(lista,x):
+    while pertence(x,lista):
+        lista.remove(x)
+
+nomes = []
+apelidos = []
 f = open ("processos.xml",encoding="utf8")
 conteudo = f.read()
 processos = re.findall(r'<processo id.+\n.+\n.+\n.+<nome>((\w+).* (\w+))<\/nome>\n.+\n.+\n.+\n.+<\/processo>',conteudo)
+#for p in processos:
+#    ps.append(list(p))
 
-print(processos[0])
+auxprocessos = processos
+for elem in auxprocessos:
+    n = elem[1]
+    a = elem[2]
+    nomes.append(n)
+    apelidos.append(a)
+
+nomesOrd = []
+apelidosOrd = []
+
+for nome in nomes:
+    k = nomes.count(nome)
+    nomesOrd.append([nome,k])
+    delete(nomes,nome)
+
+for apelido in apelidos:
+    k = apelidos.count(apelido)
+    apelidosOrd.append([apelido,k])
+    delete(apelidos,apelido)
+
+print(nomesOrd)
+
+
+
+
