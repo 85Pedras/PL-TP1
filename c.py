3cb0b2d75736258ed77a641468bf955bfc5cecac
111Bond <joaoprp111@gmail.com>
2021-03-30 16:31:59 +0100

    c

--- c.py
+++ c.py
@@ -1,39 +1,47 @@
 import sys
 import re
 
-nomes = []
+# Estruturas intermédias
+candidatos = set()
 parentesDict = dict()
 
+# Definições auxiliares
 def parentes(lista):
     for elem in lista:
         obs = elem[3]
         parentes = re.findall(r',((?i:Irmao)|(?i:Tio)|(?i:Primo)).+?\.',obs)
-        print(parentes)
         if(len(parentes) > 0):
-            nomes.append(elem[1])
+            candidatos.add(elem[1])
             for parente in parentes:
                 if parentesDict.get(parente):
                     parentesDict[parente] += 1
                 else:
                     parentesDict[parente] = 1  
         
-    print(parentesDict)
-    print(len(nomes))
+    print("Número de parentes: ",parentesDict)
+    print("Número total de candidatos com parentes eclesiasticos: ",len(candidatos))
+
+def maisFrequente(dict):
+    keys = dict.keys()
+    max = -1
+    maxKey = ""
+    for key in keys:
+        if(dict[key] > max):
+            max = dict[key]
+            maxKey = key
+
+    print("O parente eclesiastico mais frequente é o:",maxKey)
+
 
 
 #Ler todo o conteúdo do xml
 processos = open("processos.xml",encoding="utf8")
 conteudo = processos.read()
-# print(conteudo)
 
 #Número de candidatos com parentes eclesiásticos
-# candidatos = set()
 if res := re.findall(r'<processo\s(.|\n)+?<nome>([\w\s]+)<\/nome>(.|\n)+?<obs>(.*?)<\/obs>(.|\n)+?<\/processo>',conteudo):
-    #print(res)
-    # for elem in res:
-    #     print(elem)
-    #print(len(res))
     parentes(res)
+    maisFrequente(parentesDict)
 else:
     print("Erro")
 
